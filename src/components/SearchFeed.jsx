9ae9b55b85fe9560a1e9c01577a3d9fbe177c1ab
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-10-14 21:36:13 +0700

    init commit

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "../components";
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for: <span style={{ color: "#F31503" }}>videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "826ad7d916msh80c9fc386a84754p1a4882jsn101277b860f1",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
